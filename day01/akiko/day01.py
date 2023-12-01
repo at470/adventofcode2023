09f528a380d366c06736724bb9ec760ca546e033
avitus <macair>
2023-12-01 23:52:47 +0000

    added further comments, some tidying up

--- day01/akiko/day01.py
+++ day01/akiko/day01.py
@@ -1,11 +1,21 @@
 import re
-# used in part 1 and part 2
+# used in part 1 only
 def find_numbers_only_in_data_list(data_list):
 	extracted_numbers_from_string = []
 	for line in data_list:
 		temp = re.findall(r'\d+', line)
 		extracted_numbers_from_string.append(list(map(str, temp)))
 	return extracted_numbers_from_string
+
+# used in part 1 only
+def extract_and_sum_two_digits_from_line(extracted_numbers_from_string):
+	extracted_two_digit_numbers = []
+	for all_numbers_in_line in extracted_numbers_from_string:
+		first_digit = str(all_numbers_in_line[0][0])
+		last_digit = all_numbers_in_line[len(all_numbers_in_line)-1][-1]
+		two_digit_number_from_line = first_digit + last_digit
+		extracted_two_digit_numbers.append(int(two_digit_number_from_line))
+	return sum(extracted_two_digit_numbers)
 
 # used in part 2
 def find_all_numbers_in_data_list(data_list):
@@ -21,7 +31,7 @@ def find_all_numbers_in_data_list(data_list):
 		   }
 	original_extracted_values_from_string = []
 	for line in data_list:
-		temp = re.findall(r'\d{1}|one|two|three|four|five|six|seven|eight|nine', line)
+		temp = re.findall(r'\d{1}|one|two|three|four|five|six|seven|eight|nine', line) #only matching one digit with d{1}
 		original_extracted_values_from_string.append(list(map(str, temp)))
 	output =[]
 	for line in original_extracted_values_from_string:
@@ -33,16 +43,6 @@ def find_all_numbers_in_data_list(data_list):
 				numbers_per_line.append(string_to_digit_lookup[number])
 		output.append(numbers_per_line)
 	return output
-
-# used in part 1 only
-def extract_and_sum_two_digits_from_line(extracted_numbers_from_string):
-	extracted_two_digit_numbers = []
-	for all_numbers_in_line in extracted_numbers_from_string:
-		first_digit = str(all_numbers_in_line[0][0])
-		last_digit = all_numbers_in_line[len(all_numbers_in_line)-1][-1]
-		two_digit_number_from_line = first_digit + last_digit
-		extracted_two_digit_numbers.append(int(two_digit_number_from_line))
-	return sum(extracted_two_digit_numbers)
 
 # part 2
 def find_all_numbers_backwards_in_reversed_data_list(data_list):
@@ -58,7 +58,7 @@ def find_all_numbers_backwards_in_reversed_data_list(data_list):
 		   }
 	original_extracted_values_from_string = []
 	for line in data_list:
-		temp = re.findall(r'\d{1}|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin', line)
+		temp = re.findall(r'\d{1}|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin', line) #only matching one digit with d{1}
 		original_extracted_values_from_string.append(list(map(str, temp)))
 	output =[]
 	for line in original_extracted_values_from_string:
@@ -77,50 +77,46 @@ def return_list_of_one_digits(data):
 		list_of_index_zero_values.append(str(i[0]))
 	return list_of_index_zero_values
 
+
+
+
+
 # path = '/Users/akiko/Projects/adventofcode2023/adventofcode2023/day01/akiko/partonesample.csv'
 # path = '/Users/akiko/Projects/adventofcode2023/adventofcode2023/day01/akiko/parttwosample.csv'
 path = '/Users/akiko/Projects/adventofcode2023/adventofcode2023/day01/akiko/input.csv'
 raw_data = open(path, "r").read().splitlines()
-# raw_data = ['foureight48sbkkvc17zbksgvcbb', '4sixfiveone76jctmjsxdh5jrkv', 'nine296']
-# raw_data = ['mdjphcm9', 'foureightmppchbgz8lqbzqbjztwo7cksqxns', '553fivethreefour8nine']
+
+
 
 # Part 1
-# extracted_numbers_from_string = find_numbers_only_in_data_list(raw_data)
-# print(extract_and_sum_two_digits_from_line(extracted_numbers_from_string))
+extracted_numbers_from_string = find_numbers_only_in_data_list(raw_data)
+print(extract_and_sum_two_digits_from_line(extracted_numbers_from_string))
 # 55386
 
 
-#Part 2
-# raw_data = ['1sevenine', 'eninevens5', '2341', '1']
-# -> wanted-output = [19, 95, 21, 11]
+# Part 2
+# NB. 54807 IS NOT THE RIGHT ANSWER, TOO LOW -> had to rethink the whole approach, used reversing (thanks to Butch Wong)
 
-# for first digit, use find_all_numbers_in_data_list(raw_data) and use index 0
 all_first_digits_from_data = find_all_numbers_in_data_list(raw_data)
-print(all_first_digits_from_data)
 first_digit = return_list_of_one_digits(all_first_digits_from_data)
 
-# for last digit, use find_all_numbers_backwards_in_reversed_data_list and use index 0
 raw_data_reversed = []
 for i in raw_data:
 	raw_data_reversed.append(i[::-1])
-print(raw_data)
-print(raw_data_reversed)
 
 all_last_digits_from_data = find_all_numbers_backwards_in_reversed_data_list(raw_data_reversed)
 last_digit = return_list_of_one_digits(all_last_digits_from_data)
 
 if len(first_digit) == len(last_digit):
 	result = [first_digit[i]+last_digit[i] for i in range(len(first_digit))]
+	print(sum(map(int, result)))
+
 else:
 	print('some error')
 
-print(sum(map(int, result)))
-# 54824
-
-
-# output = find_all_numbers_in_data_list(raw_data)
-# print(extract_and_sum_two_digits_from_line(output))
-# 54807 IS NOT THE RIGHT ANSWER, TOO LOW
+# 54824 is correct
 
 
 
+#How to make this code better
+#Improve the function so find_all_numbers_in_data_list and find_all_numbers_backwards_in_reversed_data_list are just one function that takes in a dictionary and a regex format
