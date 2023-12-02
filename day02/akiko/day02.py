33b0bea3d64ec0f11963f802748686a724177527
avitus <macair>
2023-12-02 11:01:51 +0000

    some stuff

---
+++ day02/akiko/day02.py
@@ -0,0 +1,15 @@
+#'12 red cubes, 13 green cubes, and 14 blue cubes'
+bag_content = {'red' : 12,
+			   'green' : 13
+			   'blue' : 14}
+
+# wanted input
+games = [[{'red' : 0, 'green' : 4, 'blue' : 3}, 
+		 {'red' : 1, 'green' : 2, 'blue' : 6}, 
+		 {'red' : 0, green' : 2, 'blue' : 0}], 
+	[{'red' : 0, 'green' : 2, 'blue' : 1}, 
+		 {'red' : 1, 'green' : 3, 'blue' : 4}, 
+		 {'red' : 0, green' : 1, 'blue' : 1}]]
+
+for i in games:
+	print(i)
