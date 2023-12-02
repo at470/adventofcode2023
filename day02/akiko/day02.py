3e7a346e3f096c7a2d08e771335abed1dac1f888
avitus <macair>
2023-12-02 11:44:04 +0000

    day 2 part 1 progress

--- day02/akiko/day02.py
+++ day02/akiko/day02.py
@@ -1,15 +1,64 @@
 #'12 red cubes, 13 green cubes, and 14 blue cubes'
 bag_content = {'red' : 12,
-			   'green' : 13
+			   'green' : 13,
 			   'blue' : 14}
 
 # wanted input
-games = [[{'red' : 0, 'green' : 4, 'blue' : 3}, 
+# NB.  index of games = game id - 1
+games = [[{'red' : 4, 'green' : 0, 'blue' : 3}, 
 		 {'red' : 1, 'green' : 2, 'blue' : 6}, 
-		 {'red' : 0, green' : 2, 'blue' : 0}], 
-	[{'red' : 0, 'green' : 2, 'blue' : 1}, 
+		 {'red' : 0, 'green' : 2, 'blue' : 0}], 
+		[{'red' : 0, 'green' : 2, 'blue' : 1}, 
 		 {'red' : 1, 'green' : 3, 'blue' : 4}, 
-		 {'red' : 0, green' : 1, 'blue' : 1}]]
+		 {'red' : 0, 'green' : 1, 'blue' : 1}],
+		[{'red' : 20, 'green' : 8, 'blue' : 6}, 
+		 {'red' : 4, 'green' : 13, 'blue' : 5}, 
+		 {'red' : 1, 'green' : 5, 'blue' : 0}],
+		[{'red' : 3, 'green' : 1, 'blue' : 6}, 
+		 {'red' : 6, 'green' : 3, 'blue' : 0}, 
+		 {'red' : 14, 'green' : 3, 'blue' : 15}],
+		[{'red' : 6, 'green' : 3, 'blue' : 1}, 
+		 {'red' : 1, 'green' : 2, 'blue' : 2}] ]
 
-for i in games:
-	print(i)
+# for game_index, all_game_results in enumerate(games):
+# 	print('game id: ', game_index+1, all_game_results)
+
+# TODO
+# check that bag_content['red'] >= all_game_results[index]['red']
+# if False: 
+# 	game invalid
+# else:
+# 	bag_content['green'] >= all_game_results[index]['green']
+# 	if False:
+# 		game_invalid
+# 	else:
+# 		bag_content['blue'] >= all_game_results[index]['blue']
+# 		if False: 
+# 			game_invalid
+# 		else:
+# 			return game_index -> game_id = game_index+1
+
+
+def is_game_result_valid(bag_content, game_result):
+	# bag_content = {'red' : 12, 'green' : 13, 'blue' : 14}
+	# game_result = {'red' : 4, 'green' : 0, 'blue' : 3}
+	bag_result_comparison_var = [(bag_cube_colour, game_result[bag_cube_colour], bag_cube_num) for bag_cube_colour, bag_cube_num in bag_content.items()]
+	# bag_result_comparison_var = [('red', 20, 12), ('green', 8, 13), ('blue', 6, 14)]
+	for colour_comparison in bag_result_comparison_var:
+		print(colour_comparison, colour_comparison[1] > colour_comparison[2])
+		if colour_comparison[1] > colour_comparison[2]:
+			result = True
+			return result
+		else:
+			result = False
+	return result
+
+
+
+
+
+bag_content = {'red' : 12, 'green' : 13,'blue' : 14}
+# game_result = {'red' : 4, 'green' : 0, 'blue' : 3}
+game_result = {'red' : 20, 'green' : 8, 'blue' : 6}
+
+print(is_game_result_valid(bag_content, game_result))
